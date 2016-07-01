7ee4ec7908a4554c4913fac41c91eca45ad6d09b
Jarek Plonski <jarek@codequest.com>
2016-07-01 21:49:11 +0200

    Bring back compatibility with Test::Unit

--- lib/sunspot_matchers/test_helper.rb
+++ lib/sunspot_matchers/test_helper.rb
@@ -1,10 +1,14 @@
 require 'sunspot'
-require 'minitest/autorun'
 require File.expand_path('../matchers', __FILE__)
 require File.expand_path('../sunspot_session_spy', __FILE__)
 module SunspotMatchers
   class HaveSearchParamsForSession
-    include MiniTest::Assertions
+    if const_defined? 'MiniTest'
+      include MiniTest::Assertions
+    else
+      include Test::Unit::Assertions
+    end
+
 
     def initialize(session, method, *args, &block)
       @session = session
